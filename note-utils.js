86cba5779acddddbdbfaa8a388d8e442cda515c2
Raphaël Léger <raphael@nodixo.com>
2023-04-17 16:21:33 +0200

    show last note played

--- note-utils.js
+++ note-utils.js
@@ -8,27 +8,64 @@
  *	https://www.opensource.org/licenses/bsd-license.php
  */
 
-function hzToNote(freq)
-{
-	var note = 12 * ( Math.log(freq / 440) / Math.log(2) );
+function hzToNote(freq) {
+	var note = 12 * (Math.log(freq / 440) / Math.log(2));
 	return Math.round(note) + 49;
 }
 
-function noteString(note)
-{
-	const notes = [ "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#" ];
-	const letter = notes[ (note + 11) % notes.length ];
-	const octave = Math.floor( (note - 49) / notes.length ) + 4;
-	
-	return letter + (letter.length < 2 ? '.' : '') + octave;
+function noteString(note) {
+	const notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+	const notesEuropean = ["LA", "LA#", "SI", "DO", "DO#", "RE", "RE#", "MI", "FA", "FA#", "SOL", "SOL#"];
+	const letter = notes[(note + 11) % notes.length];
+	const letterEuropean = notesEuropean[(note + 11) % notesEuropean.length];
+	const octave = Math.floor((note - 49) / notes.length) + 4;
+
+	const name = letterEuropean + octave;
+	const sharp = letter.length > 1 ? true : false;
+
+	let abcjs = '';
+	let abcjsLetter = letter;
+	let abcjsOctave = octave;
+	if (letter === "A" || letter === "B" || letter === "A#" || letter === "B#") {
+		abcjsOctave = abcjsOctave - 1;
+	}
+
+	if (abcjsOctave === 2) {
+		abcjsLetter = `${letter},,`
+	} else if (abcjsOctave === 3) {
+		abcjsLetter = `${letter},`
+	} else if (abcjsOctave === 4) {
+		abcjsLetter = letter
+	} else if (abcjsOctave === 5) {
+		abcjsLetter = `${letter.toLowerCase()}`
+	} else if (abcjsOctave === 6) {
+		abcjsLetter = `${letter.toLowerCase()}'`
+	} else if (abcjsOctave === 7) {
+		abcjsLetter = `${letter.toLowerCase()}''`
+	}
+
+	if (sharp) {
+		abcjsLetter = `^${abcjsLetter.charAt(0)}`;
+	}
+	abcjs = `${abcjsLetter}2`;
+
+	return {
+		name,
+		letter,
+		letterEuropean,
+		octave,
+		abcjs
+	};
 }
 
-function hzToNoteString(freq)
-{
-	return noteString( hzToNote(freq) );
+function hzToNoteObject(freq) {
+	return noteString(hzToNote(freq));
 }
 
-function noteToHz(note)
-{
-	return 440 * Math.pow(2, (note-49)/12 );
+function hzToNoteString(freq) {
+	return hzToNoteObject(freq).name;
+}
+
+function noteToHz(note) {
+	return 440 * Math.pow(2, (note - 49) / 12);
 }
---
+++ note-visualiser.js
@@ -0,0 +1,25 @@
+function renderScoreResult(note) {
+    let treble = ''
+    let bass = '';
+
+    if (note.name) {
+        if (note.octave >= 4 && note.name !== "LA4" && note.name !== "SI4") {
+            treble = note.abcjs
+        } else {
+            bass = note.abcjs
+        }
+    }
+
+
+    var trebleAndBass = `
+X:1
+M: 4/4
+K:treble
+V:V1 clef=treble
+V:V2 clef=bass
+[V:V1] ${treble}
+[V:V2] ${bass}
+`;
+
+    ABCJS.renderAbc('scoreResult', trebleAndBass, { scale: 3 });
+}
